4789f7e4c00b7d765f95e20f09d03fe8edf9e02d
小车神XCShey <XCShey@outlook.com>
2024-05-02 11:30:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '🎉欢迎来到小车神hey的博客🎉(B站)',url: 'https://b23.tv/YaCS8uN' },
-    { title: '🎉Welcome to XCShey blog🎉(YouTube)',url: 'https://youtube.com/@hey-XCR?si=vCH4LAk8_pt1HyfI' }
+    { title: '🎉欢迎来到小车神hey的博客🎉                                      （B站）',url: 'https://b23.tv/YaCS8uN' },
+    { title: '🎉Welcome to XCShey blog🎉                                     （YouTube）',url: 'https://youtube.com/@hey-XCR?si=vCH4LAk8_pt1HyfI' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -15,17 +15,17 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '寄术玩家',
-  HEO_HERO_TITLE_2: '科技能手',
-  HEO_HERO_TITLE_3: '双重身份',
-  HEO_HERO_TITLE_4: 'TheBXCShey.com',
-  HEO_HERO_TITLE_5: '点此进入我的主页',
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选'， url: '/tag/必看精选' }，
-  HEO_HERO_CATEGORY_2: { title: '热门文章'， url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程'， url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
