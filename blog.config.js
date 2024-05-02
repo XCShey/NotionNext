56b1a001496f033eced453daded4727ae30d9d41
小车神XCShey <XCShey@outlook.com>
2024-05-02 13:23:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     '🎉你好，我是小车神hey🎉, 🎉Hi，I am XCShey🎉,🎉欢迎来到我的博客🎉🎉-🎉🎉Welcome to my blog🎉',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '小车神XCShey', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '来了老弟！我是小车神hey', // 作者简介
