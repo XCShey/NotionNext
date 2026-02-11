563a0e1e86b784c8895bf58416bd843bf1264102
小车神XCShey <XCShey@outlook.com>
2026-02-11 14:55:17 +0800

    Update notice bar and hero section links

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '🎉欢迎来到XCShey的博客🎉', url: '' },
-    { title: '🎉Welcome to XCShey blog🎉', url: '' }
+    { title: '🎉欢迎来到XCShey的博客🎉', url: 'https://hp.thebxcshey.top' },
+    { title: '🎉点此进入我的主页🎉', url: 'https://hp.thebxcshey.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -22,15 +22,15 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '追求自由',
   HEO_HERO_TITLE_3: 'THEBXCSHEY.TOP',
   HEO_HERO_TITLE_4: '敢于探索',
-  HEO_HERO_TITLE_5: '点击进入我的主页',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '点击进入聚合页',
+  HEO_HERO_TITLE_LINK: 'https://www.thebxcshey.top/webp',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '我的主页', url: 'https://hp.thebxcshey.top' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
